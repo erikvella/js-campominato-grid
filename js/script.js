6a60b00a8a922f6df21713d6950cc02549404d2a
erikvella <vella.erik1@gmail.com>
2023-09-18 19:24:06 +0100

    [add] btn

--- js/script.js
+++ js/script.js
@@ -6,18 +6,70 @@
 
 // 4 al click del quadrato aggiungo la classe .clicked 
 
+const container = document.querySelector('.container');
+const menu =      document.querySelector('.top-menu');
+reset()
+
+init()
 
 
 
-for(let i = 1 ; i <= 100 ; i++){
-  const sqaure = createSquare();
+
+
+
+
+// da qui iniziano tutte le mie funzioni 
+
+function init(){
+  for(let i = 1 ; i <= 100 ; i++){
+    const sqaure = createSquare(i);
+    // aggiungo gli eventi al click dei quadrati
+    sqaure.addEventListener('click' , function(){
+    
+    console.log(this);
+    // leggo la proprietà custom attraverso questo ID
+    console.log(this._squareID);
+    // this.innerText legge in contenuto del mio elemento
+    console.log(this.innerText);
+
+    this.classList.toggle('clicked');
+    this.classList.toggle('oddEven'(this._squareID));
+    })
+    
+    container.append(sqaure);
+  }
+  
 }
 
-
-
-function createSquare(){
+function createSquare(index){
   //  devo creare lo square
   const newSquare = document.createElement('div');
   newSquare.className = 'square';
+  // aggiungo una proprietà custom con l'indice (numero del quadrato)
+  newSquare._squareID = index;
+  newSquare.innerHTML = `<span>${index}</span>`;
   return newSquare;
+}
+
+
+function createBtnStart(){
+  // devo creare un bottone di Start
+  const startBtn = document.createElement('button');
+  startBtn.innerHTML = 'START';
+  startBtn.addEventListener('click' , function(){
+    menu.innerHTML = '';
+    container.innerHTML = '';
+    init();
+  })
+  return startBtn;
+}
+function reset(){
+  container.innerHTML = '';
+  menu.append(createBtnStart());
+}
+
+function oddEven(id){
+  if(id % 2)return 'odd';
+  return 'even';
+
 }
